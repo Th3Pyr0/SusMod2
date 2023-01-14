53a029f46a3d6d21fe7a310e725697afa3582c7a
The Pyro <78774666+Th3Pyr0@users.noreply.github.com>
2023-01-14 00:04:51 -0800

    Add files via upload

---
+++ OldShit.cs
@@ -0,0 +1,14 @@
+using Terraria.ModLoader;
+
+namespace OldShit
+{
+    public class OldShit : Mod
+    {
+        internal class NPCs
+        {
+            internal class Clobberglobber
+            {
+            }
+        }
+    }
+}
